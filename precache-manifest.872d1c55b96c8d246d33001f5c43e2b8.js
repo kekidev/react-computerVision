fc5b650903a87fd69c33f5648bacaa9acc4dd2c7
wevbute <baejaehwan1125@gmail.com>
2021-04-12 23:37:49 +0900

    Updates

---
+++ precache-manifest.872d1c55b96c8d246d33001f5c43e2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724b37ee04273e3a0d2b63047d20924c",
+    "url": "/react-computerVision/index.html"
+  },
+  {
+    "revision": "69a312b9ca0f5b3ab965",
+    "url": "/react-computerVision/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9f4913faf2b5e4783a0",
+    "url": "/react-computerVision/static/js/2.dcd97d6d.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/react-computerVision/static/js/2.dcd97d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a312b9ca0f5b3ab965",
+    "url": "/react-computerVision/static/js/main.a716934e.chunk.js"
+  },
+  {
+    "revision": "17ab017194153e5cd74d",
+    "url": "/react-computerVision/static/js/runtime-main.79692551.js"
+  }
+]);
